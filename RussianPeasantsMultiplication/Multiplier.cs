4db9fc9020ca384c3122629154a144aae6b9c4c0
toby-s1 <tobytronics@gmail.com>
2014-02-15 18:22:29 +0000

    Initial working version with tests

---
+++ RussianPeasantsMultiplication/Multiplier.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RussianPeasantsMultiplication
+{
+    public class Multiplier
+    {
+        public int Multiply(int first, int second)
+        {
+            var numbers = new Dictionary<int, int>();
+
+            while (true)
+            {
+                if (first % 2 != 0) { numbers.Add(first, second); }
+                if (first == 1) { break; }
+
+                first = (int)Math.Floor(first / 2.0);
+                second = second * 2;
+            }
+  
+            var total = numbers.Values.Aggregate((sum, val) => sum + val);
+            return total;
+        }
+    }
+}
---
+++ RussianPeasantsTests/MultiplierTestShould.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RussianPeasantsMultiplication;
+
+namespace RussianPeasantsTests
+{
+    [TestFixture]
+    public class MultiplierTestShould
+    {
+        private readonly Multiplier multiplier = new Multiplier();
+        
+        [TestCase(1,1, Result = 1)]
+        [TestCase(2, 2, Result = 4)]
+        [TestCase(1, 2, Result = 2)]
+        [TestCase(55, 43, Result = 2365)]
+        public int correctly_multiply_specified_integers(int first, int second)
+        {
+            return multiplier.Multiply(first, second);
+        }
+
+        [Test]
+        public void correctly_multiply_any_integers([Random(1, 1000, 10)]int first, [Random(1, 1000, 10)]int second)
+        {
+            var expected = first*second;
+            Assert.AreEqual(expected, multiplier.Multiply(first, second), "Multiplier returned incorrect answer");
+        }
+
+    }
+}
