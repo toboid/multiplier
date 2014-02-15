1e871de39837f7b7d1c8475265be70d547787ed9
toby-s1 <tobytronics@gmail.com>
2014-02-15 18:54:53 +0000

    Very simple console program to utilise multiplier

--- RussianPeasantsMultiplication/Program.cs
+++ RussianPeasantsMultiplication/Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace RussianPeasantsMultiplication
+namespace Multiplier
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var multiplier = new Multiplier();
+
+            Console.WriteLine("Enter first integer");
+            var first = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second integer");
+            var second = int.Parse(Console.ReadLine());
+
+            var answer = multiplier.Multiply(first, second);
+            Console.WriteLine("{0} * {1} = {2}", first, second, answer);
         }
     }
 }
