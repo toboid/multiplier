2ef6006a3570d58ca3ad0cbb3eb83c5160c133ab
toby-s1 <tobytronics@gmail.com>
2014-02-16 08:34:12 +0000

    Tweaking unit tests

--- MultiplierTests/Multiplier_should.cs
+++ MultiplierTests/Multiplier_should.cs
@@ -10,14 +10,14 @@ namespace MultiplierTests
         [TestCase(1,1, Result = 1)]
         [TestCase(2, 2, Result = 4)]
         [TestCase(1, 2, Result = 2)]
-        [TestCase(55, 43, Result = 2365)]
+        [TestCase(2, 1, Result = 2)]
         public int correctly_multiply_specified_integers(int first, int second)
         {
             return multiplier.Multiply(first, second);
         }
 
         [Test]
-        public void correctly_multiply_any_integers([Random(1, 1000, 10)]int first, [Random(1, 1000, 10)]int second)
+        public void correctly_multiply_any_integers_below_1000([Random(1, 1000, 10)]int first, [Random(1, 1000, 10)]int second)
         {
             var expected = first*second;
             Assert.AreEqual(expected, multiplier.Multiply(first, second), "Multiplier returned incorrect answer");
